35b34adb0386e0a4449367e91b91201cbea541f8
Savio Andrade <23692454+andradesavio9073@users.noreply.github.com>
2023-10-14 13:56:48 +0100

    implemented texture spliting

--- Scaffold/src/main.rs
+++ Scaffold/src/main.rs
@@ -1,8 +1,10 @@
-use std::{path::PathBuf, fs, env::current_dir};
+use std::{path::PathBuf, fs::{self, File}, env::current_dir};
 use anyhow::Result;
 use clap::Parser;
 use fs_extra::dir::{CopyOptions, copy};
-
+use glob::glob;
+use image::{RgbaImage, GrayImage, Luma};
+use itertools::izip;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -59,21 +61,118 @@ fn copy_source(source_path: PathBuf, root_path: &PathBuf, symlink_source: bool) 
     } else{
         let mut options = CopyOptions::new();
         options.content_only= true;
+        options.overwrite=true;
          fs::create_dir_all(source_folder.clone())?;
         copy(source_path, source_folder.clone(), &options)?;
     }
     Ok(())
 }
 
-fn gen_tex(source_path: PathBuf, root_path: &PathBuf, tex_only: bool){
+pub trait Decompose {
+    fn split_channels(&self) -> [GrayImage; 4];
+}
+
+
+impl Decompose for RgbaImage {
+    fn split_channels(&self) -> [GrayImage; 4] {
+        let mut red = GrayImage::new(self.width(), self.height());
+        let mut green = red.clone();
+        let mut blue = red.clone();
+        let mut alpha = red.clone();
+        izip!(
+            red.pixels_mut(),
+            green.pixels_mut(),
+            blue.pixels_mut(),
+            alpha.pixels_mut(),
+            self.pixels()
+        )
+        .for_each(|(r, g, b, a, rgba)| {
+            *r = Luma([rgba[0]]);
+            *g = Luma([rgba[1]]);
+            *b = Luma([rgba[2]]);
+            *a = Luma([rgba[3]]);
+        });
+
+        [red, green, blue, alpha]
+    }
+}
+
+pub trait Compose {
+    fn join_channels(&mut self, channels: [GrayImage; 4]) -> ();
+}
+
+impl Compose for RgbaImage {
+    fn join_channels(&mut self, channels: [GrayImage; 4]) -> () {
+        
+        izip!(
+            channels[0].pixels(),
+            channels[1].pixels(),
+            channels[2].pixels(),
+            channels[3].pixels(),
+            self.pixels_mut()
+        )
+        .for_each(|(r, g, b, a, rgba)| {
+            rgba[0] = r[0];
+            rgba[1] = g[0];
+            rgba[2] = b[0];
+            rgba[3] = a[0];
+        });
+
+    }
+}
+
+fn grab_tex(source: PathBuf,target: PathBuf,  image_type: &str, split_alpha: bool) -> Result<()>{
+    let mut glob_pattern = "*".to_owned();
+    glob_pattern.push_str(image_type);
+    glob_pattern.push_str(".dds");
+    let full_glob_pattern = source.join(glob_pattern);
+    for entry in glob(full_glob_pattern.to_str().expect("Failed to parse str"))?{
+        let path = entry?.clone();
+        let filename = path.file_stem().expect("Failed to get Filename").to_str().expect("Failed to parse str");
+        let mut reader = File::open(&path).unwrap();
+
+        let dds = ddsfile::Dds::read(&mut reader).unwrap();
+        let mut image:RgbaImage = image_dds::image_from_dds(&dds, 0).unwrap();
+        let mut rgba = image.split_channels();
+        
+        let out_file = target.clone();
+        
+        if split_alpha{
+            let mut target_file_name = "".to_owned();
+            target_file_name.push_str(filename);
+            target_file_name.push_str("Alpha");
+            target_file_name.push_str(".png");
+            let alpha = &rgba[3];
+            alpha.save(out_file.clone().join(target_file_name))?;
+        }
+        let mut target_file_name = "".to_owned();
+        target_file_name.push_str(filename);
+        target_file_name.push_str("Flat");
+        target_file_name.push_str(".png");
+        rgba[3].pixels_mut().for_each(|p| p[0] = 255);
+        image.join_channels(rgba);
+        image.save(out_file.clone().join(target_file_name))?;
+    }
+    Ok(())
+}
+
+
+fn gen_tex(source_path: PathBuf, root_path: &PathBuf, tex_only: bool) -> Result<()>{
     let source:PathBuf;
     if tex_only{ 
         source = source_path;
     } else{
         source = root_path.clone().join("Source");
     }
-    println!("Tex Source: {:#?}", source);
-    todo!("Generate Flat Textures and Alpha Maps")
+    let tex_path = root_path.clone().join("Textures");
+    fs::create_dir_all(tex_path.clone())?;
+
+    grab_tex(source.clone(), tex_path.clone(), "Diffuse", true)?;
+    grab_tex(source.clone(), tex_path.clone(), "LightMap", true)?;
+    grab_tex(source.clone(), tex_path.clone(), "NormalMap", false)?;
+    Ok(())
+
+    // todo!("Generate Flat Textures and Alpha Maps")
 }
 
 fn main() {
@@ -92,5 +191,8 @@ fn main() {
             Err(err) => panic!("Error: Failed to Copy Source Dir: {}", err)
         }
     }
-    gen_tex(args.source.clone(), &root_path, args.tex_only);
+    match gen_tex(args.source.clone(), &root_path, args.tex_only){
+        Ok(_) => (),
+        Err(err) => panic!("Error: Failed to Generate Textures: {}", err)
+    }
 }
